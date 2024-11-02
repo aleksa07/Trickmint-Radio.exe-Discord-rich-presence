59ece1a5e5dae059b42e2f1aa3b338f705272ed1
aleksa07 <33092759+aleksa07@users.noreply.github.com>
2024-11-02 11:16:11 +0100

    Create Trickmint radio.exe Source.py

---
+++ Trickmint radio.exe Source.py
@@ -0,0 +1,170 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+from pypresence import Presence
+import pickle
+import os
+import keyboard
+import time
+
+configfile = 'config.pkl'
+
+def loadconfig():
+    if os.path.exists(configfile):
+        with open(configfile, 'rb') as file:
+            return pickle.load(file)
+    else:
+        return {}
+
+def saveconfig(config):
+    with open(configfile, 'wb') as file:
+        pickle.dump(config, file)
+        
+config = loadconfig()
+
+setup = True
+
+if config.get('Setup'):
+    if config.get('Browser') == 'Firefox':
+        driver = webdriver.Firefox()
+        driver.get('https://trickmint.gay/')
+    elif config.get('Browser') == 'Chrome':
+        driver = webdriver.Chrome()
+        driver.get('https://trickmint.gay/')
+    elif config.get('Browser') == 'Edge':
+        driver = webdriver.Edge()
+        driver.get('https://trickmint.gay/')
+    elif config.get('Browser') == 'Safari':
+        driver = webdriver.Safari()
+        driver.get('https://trickmint.gay/')
+else:
+    print("Doing Setup")
+    setup = False
+
+
+def Setup():
+    num = input("Which browser would you like to use: 1 = Firefox, 2 = Chrome, 3 = Edge, 4 = Safari (untested): ") 
+    try:
+        if int(num) == 1:
+            config['Browser'] = 'Firefox'
+            config['Setup'] = True
+            saveconfig(config)
+            return webdriver.Firefox()
+        elif int(num) == 2:
+            config['Browser'] = 'Chrome'
+            config['Setup'] = True
+            saveconfig(config)
+            return webdriver.Chrome()
+        elif int(num) == 3:
+            config['Browser'] = 'Edge'
+            config['Setup'] = True
+            saveconfig(config)
+            return webdriver.Edge()
+        elif int(num) == 4:
+            config['Browser'] = 'Safari'
+            config['Setup'] = True
+            saveconfig(config)
+            return webdriver.Safari()
+        else:
+            print("Please enter a number from 1 to 4. Try again.")
+            return Setup()
+    except Exception as e:
+        print(f"Invalid input or browser not installed. Error: {e}")
+        config['Setup'] = False
+        saveconfig(config)
+        return Setup()
+
+if not setup:
+    driver = Setup()
+    driver.get('https://trickmint.gay/')
+
+
+play = True
+client_id = '1299741681452843139'
+discord_presence = Presence(client_id)
+
+def Connect():
+    try:
+        discord_presence.connect()
+    except Exception as e:
+        print("Unable to connect to Discord. Retrying in 5 seconds...")
+        time.sleep(5)
+        Connect()
+Connect()
+
+def playbutton():
+    try:
+        play_button = driver.find_element(By.ID, 'playtoggle')
+        play_button.click()
+    except Exception as e:
+        print(f"Error clicking play button: {e}")
+
+def Song():
+    global play
+    if not play:
+        play = True
+        playbutton()
+    else:
+        play = False 
+        playbutton()   
+
+def Spearmint():
+    driver.execute_script("refreshSpearmint();")
+    
+def Skipsong():
+    driver.execute_script("skipSong();")
+
+keyboard.add_hotkey("alt+f9", Song)
+keyboard.add_hotkey("alt+f10", Skipsong)
+keyboard.add_hotkey("alt+f11", Spearmint)
+
+def whatsong():
+    global play
+    if play:
+        song = driver.execute_script("return titleTxt.textContent;")
+        return song
+    
+
+def getimgurl():
+    img = driver.find_element(By.ID, "spearmint")
+    img = img.get_attribute("src")
+    return img
+
+def whodraw():
+    draw = driver.find_element(By.ID, "spearCred")
+    draw = draw.text
+    if draw == "by me!":
+        draw = "by trickmint"
+    return draw
+
+playbutton()
+
+while True:
+    try:
+        Image = getimgurl()
+        CREDITS = whodraw()
+        current_song = whatsong()
+        time.sleep(1)
+        if play:
+            discord_presence.update(
+                state=current_song, 
+                details="Now Playing",
+                large_image="radioicon",
+                large_text="Trickmint Radio",
+                small_image=Image,
+                small_text=f"spearmint {CREDITS}",
+                buttons=[{"label": "Trickmint's website", "url": "https://trickmint.gay/"}]
+            )
+        else:
+            discord_presence.update(
+                state="Not Listening to anything",
+                details="Boo",
+                large_image="radioicon",
+                large_text="Trickmint Radio",
+                small_image=Image,
+                small_text=f"spearmint {CREDITS}",
+                buttons=[{"label": "Trickmint's website", "url": "https://trickmint.gay/"}]
+            )
+    except Exception as e:
+        print(f"Error on discord rich presence: {e}")
